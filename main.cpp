f8bcc8fab1f615572bd533284ff9ece6b5a31d76
Omkar Acharekar <omkaracharekar12@gmail.com>
2021-01-24 14:00:00 +0100

    menu bar

--- main.cpp
+++ main.cpp
@@ -21,6 +21,479 @@
 using namespace std;
 #define pi 3.142857
 
+//Value of RGB while Drawing
+float red = 0.0, green = 0.0, blue = 0.0;
+
+int ptsize = 2;
+int brushSize = 4;
+int edge = 3;
+int eraserSize = 1;
+int tmpx, tmpy; // store the first point when shape is line, rectangle or circle
+bool isSecond = false;
+/*
+When shape is line , rect , circle after storing the first point we make isSecond =true ......
+so as to take new values of x , y and give call to line , or rect , or circle with 4 parameters
+, old points stores in tmpx ,tmpy and new points stored in x, y
+*/
+bool isfilled = false;
+
+bool isRandom = false; //When its true it selects random color
+bool isEraser = false; //When we have selected Eraser
+
+float window_w = 500;
+float window_h = 500;
+bool isRadial = false;
+
+//Initially we have kept shape as point
+int shape = 1; // 1:point, 2:line, 3:rectangle, 4:circle, 5:brush
+
+vector<Point> points;		 // store all the points until clear
+std::list<int> undoHistory;	 // record for undo, maximum 20 shapes in history
+std::list<int> redoHistory;	 // record for redo, maximum 20 shapes in history
+std::vector<Point> redoDots; // store the dots after undo temporaly
+
+line L;
+circle C;
+Rectang R;
+Polyg P1;
+
+void printGuide();
+
+void clear()
+{
+	points.clear();
+	undoHistory.clear();
+	redoDots.clear();
+	redoHistory.clear();
+	glClear(GL_COLOR_BUFFER_BIT);
+	glutSwapBuffers();
+	cout << "[Info] The window is cleared successfully.\n";
+}
+
+void quit()
+{
+	cout << "Thank you for using this Paint tool! Goodbye!" << endl;
+	exit(0);
+}
+
+void undo()
+{
+	if (undoHistory.size() > 0)
+	{
+		if (undoHistory.back() != points.size() && redoHistory.back() != points.size())
+		{
+			redoHistory.push_back(points.size());
+		}
+
+		int numRemove = points.size() - undoHistory.back();
+
+		for (int i = 0; i < numRemove; i++)
+		{
+			redoDots.push_back(points.back());
+			points.pop_back();
+		}
+		redoHistory.push_back(undoHistory.back());
+		undoHistory.pop_back();
+	}
+	else
+	{
+		cout << "[Warning] Cannot undo. This is the first record in the history.\n";
+	}
+}
+
+void redo()
+{
+	if (redoHistory.size() > 1)
+	{
+		undoHistory.push_back(redoHistory.back());
+		redoHistory.pop_back();
+		int numRemove = redoHistory.back() - points.size();
+		for (int i = 0; i < numRemove; i++)
+		{
+			points.push_back(redoDots.back());
+			redoDots.pop_back();
+		}
+	}
+	else
+	{
+		cout << "[Warning] Cannot redo. This is the last record in the history.\n";
+	}
+}
+
+/*
+FOR GLUT : mouse_x and mouse_y
+Top left corner is the origin
+As we move on right x increases
+As we move down y increases
+
+
+But For OPENGL
+Bottom Left Corner is the origin
+As we move on right x increases
+As we move up y increases
+
+*/
+void drawPoint(int mousex, int mousey)
+{
+	Point newPoint(mousex, window_h - mousey, isEraser ? 1.0 : red, isEraser ? 1.0 : green, isEraser ? 1.0 : blue);
+	points.push_back(newPoint);
+
+	/*
+Its taking in 2 parameters a)mouse_x, mouse_y....
+red ,green ,blue are global variables.........
+isEraser tell weather we have selected eraser or not......
+Becoz if eraser is selected then value of rgb at that point must be(1,1,1)..
+
+So it simply sees the point and color of the point
+Makes a new object of class POINT........Initializes the object by seeing the values........ and pushes the POINT object in dots vector.............
+*/
+}
+
+void line::drawLine(int x1, int y1, int x2, int y2)
+{
+	if (x1 == x2)
+	{
+		for (float i = (y1 < y2 ? y1 : y2); i <= (y1 > y2 ? y1 : y2); i += 0.01)
+		{
+			drawPoint(x1, i);
+		}
+		return;
+	}
+	else if (y1 == y2)
+	{
+		for (float i = (x1 < x2 ? x1 : x2); i <= (x1 > x2 ? x1 : x2); i += 0.01)
+		{
+			drawPoint(i, y1);
+		}
+		return;
+	}
+	else
+	{
+		for (float t = 0; t <= 1; t += .001)
+		{
+			float x = x1 + (x2 - x1) * t;
+			float y = y1 + (y2 - y1) * t;
+			drawPoint(x, y);
+		}
+	}
+	return;
+}
+
+//x1,y1 and x2,y2 must be any 2 diagonal points of rectangle
+void Rectang ::drawRectangle(int x1, int y1, int x2, int y2)
+{
+	drawLine(x1, y1, x2, y1);
+	drawLine(x2, y1, x2, y2);
+	drawLine(x2, y2, x1, y2);
+	drawLine(x1, y2, x1, y1);
+}
+
+/*
+void drawRectangle_filled(int x1, int y1, int x2, int y2)
+{
+    if(y1>y2)
+    {
+        for(float i=y2 ; i<=y1 ;i++)
+        {
+            L.drawLine(x1, i, x2, i);
+        }
+    }
+    else
+    {
+        for(float i=y1 ; i<=y2 ;i++)
+        {
+            L.drawLine(x1, i, x2, i);
+        }
+    }
+}
+*/
+
+void circle::drawCircle(int x1, int y1, int x2, int y2)
+{
+	float r = sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
+	float x, y;
+
+	for (float i = 0; i < (2 * pi); i += 0.001)
+	{
+		//Using Parametric Equation of circle
+		// circle is defined as x=r*cos(i) and y=r*sin(i)
+		x = x1 + r * cos(i);
+		y = y1 + r * sin(i);
+		drawPoint(x, y);
+	}
+}
+/*
+void drawCircle_Filled(int x1, int y1, int x2, int y2)
+{
+    float r = sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
+    float x,y;
+
+
+    for(float j=0 ; j<=r ; j++)
+    {
+        for (float i = 0; i < (2 * pi); i += 0.001)
+        {
+            //Using Parametric Equation of circle
+            // circle is defined as x=r*cos(i) and y=r*sin(i)
+            x = x1 + j * cos(i);
+            y = y1 + j * sin(i);
+            drawPoint(x, y);
+        }
+    }
+}
+*/
+
+void Polyg::draw_polygon(int x1, int y1, int x2, int y2)
+{
+	float x, y, m, n, i;
+	x = x1;
+	y = y1;
+	float a = sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
+	float interior = (pi * (edge - 2) / edge);
+	float k = pi - interior;
+	//Writing in general function{}
+	for (int j = 0; j < edge; j++)
+	{
+		m = x;
+		n = y;
+		L.drawLine(m, n, m + a * cos(k * j), n + a * sin(k * j));
+		x = m + a * cos(k * j);
+		y = n + a * sin(k * j);
+	}
+}
+
+/*
+//It draws a dot of white color , wherever we want to erase
+//if eraser size is 1 , then it will erase  square of 3*3
+//if eraser size is 2 , then it will erase  square of 5*5
+//if eraser size is x , then it will erase  square of (2x+1)*(2x+1)
+*/
+void erase(int x, int y)
+{
+	for (int i = -eraserSize; i <= eraserSize; i++)
+	{
+		for (int j = -eraserSize; j <= eraserSize; j++)
+		{
+			drawPoint(x + i, y + j);
+		}
+	}
+}
+
+void drawRadialBrush(int x, int y)
+{
+	int xc = glutGet(GLUT_WINDOW_WIDTH) / 2;
+	int yc = glutGet(GLUT_WINDOW_HEIGHT) / 2;
+	int dx, dy;
+
+	dx = xc - x;
+	dy = yc - y;
+
+	/*
+	Taking img wrt x-axis
+	Taking img wrt y-axis
+	Taking img wrt origin => the centre of the screen
+
+	And then interchanging x and y for all these 4 points
+	*/
+	drawPoint(xc + dx, yc + dy);
+	drawPoint(xc - dx, yc + dy);
+	drawPoint(xc + dx, yc - dy);
+	drawPoint(xc - dx, yc - dy);
+
+	drawPoint(xc + dy, yc + dx);
+	drawPoint(xc - dy, yc + dx);
+	drawPoint(xc + dy, yc - dx);
+	drawPoint(xc - dy, yc - dx);
+}
+
+/*
+Same as draw dot
+//It actually gives a call to drawDot Function
+//The difference is , drawBrush is used for AirBrush
+//So when size of AirBrush is 4px , it puts 4 points very near to our selected (x,y)
+//So when size of AirBrush is 8px , it puts 8 points very near to our selected (x,y)
+//So when size of AirBrush is 12px , it puts 12 points very near to our selected (x,y)
+//So when size of AirBrush is 16px , it puts 16 points very near to our selected (x,y)
+//Its using rand() to get a random point very near to our point
+*/
+void drawBrush(int x, int y)
+{
+	for (int i = 0; i < brushSize; i++)
+	{
+		int randX = rand() % (brushSize + 1) - brushSize / 2 + x;
+		int randY = rand() % (brushSize + 1) - brushSize / 2 + y;
+		drawPoint(randX, randY);
+	}
+	/*
+	Larger the Size of the Brush , More the Nos of Dots it prints in one Click
+	Also how long the dots would be printed from the position of our click , would again depend on size of the brush...   % (brushSize + 1)
+	If size of brush .., width would be more else the width would be less......   rand() % (brushSize + 1) ..when brushSize is more ,ts value would also be more
+
+	The C library function int rand(void) returns a pseudo-random number in the range of 0 to RAND_MAX.
+    RAND_MAX is a constant whose default value may vary between implementations but it is granted to be at least 32767.
+
+
+	Due to this rand() % (brushSize + 1) will always be positive ...Hence the RanDom Points Will Only be on One Side of Our pt. X,Y....
+	//To avoid this we are doing - brushSize / 2.....so the points would be evenly distributed around the point where we clicked
+    */
+}
+
+
+
+//GLUTMOTIONFUNC gives call to The motion , when the mouse moves within the window while one or more mouse buttons are pressed.
+void motion(int x, int y)
+{
+	if (isEraser)
+		erase(x, y);
+	else
+	{
+		if (shape == 1)
+		{
+			if (isRadial)
+				drawRadialBrush(x, y);
+			else
+				drawPoint(x, y);
+		}
+		if (shape == 5)
+			drawBrush(x, y);
+	}
+}
+
+//When a user presses and releases mouse buttons in the window , glutMouseFunc sets the mouse callback for the current window.
+void mouse(int bin, int state, int x, int y)
+{
+	if (bin == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+	{
+		if (isRandom)
+		{
+			red = float(rand()) / float(RAND_MAX);
+			green = float(rand()) / float(RAND_MAX);
+			blue = float(rand()) / float(RAND_MAX);
+		}
+
+		if (isEraser)
+		{
+			undoHistory.push_back(points.size());
+			erase(x, y);
+		}
+		else
+		{
+			if (shape == 1)
+			{
+				undoHistory.push_back(points.size());
+				if (isRadial)
+					drawRadialBrush(x, y);
+				else
+					drawPoint(x, y);
+			}
+			else if (shape == 5)
+			{
+				undoHistory.push_back(points.size());
+				drawBrush(x, y);
+			}
+			else
+			{
+				if (!isSecond)
+				{
+					tmpx = x;
+					tmpy = y;
+					isSecond = true;
+				}
+				else
+				{
+					if (undoHistory.back() != points.size())
+						undoHistory.push_back(points.size());
+					if (shape == 2)
+						L.drawLine(tmpx, tmpy, x, y);
+					else if (shape == 3)
+						R.drawRectangle(tmpx, tmpy, x, y);
+					else if (shape == 4)
+						C.drawCircle(tmpx, tmpy, x, y);
+					else if (shape == 6)
+						P1.draw_polygon(tmpx, tmpy, x, y);
+					isSecond = false;
+				}
+			}
+		}
+
+		if (undoHistory.size() > 20)
+		{
+			undoHistory.pop_front();
+		}
+	}
+}
+
+void processColourMenu(int value)
+{
+	isSecond = false;
+	isEraser = false;
+	isRandom = false;
+
+	switch (value)
+	{
+	case 1: // red
+		red = 1.0;
+		green = 0.0;
+		blue = 0.0;
+		break;
+	case 2: // green
+		red = 0.0;
+		green = 1.0;
+		blue = 0.0;
+		break;
+	case 3: // blue
+		red = 0.0;
+		green = 0.0;
+		blue = 1.0;
+		break;
+	case 4: // purple
+		red = 0.5;
+		green = 0.0;
+		blue = 0.5;
+		break;
+	case 5: // yellow
+		red = 1.0;
+		green = 1.0;
+		blue = 0.0;
+		break;
+	case 6: // black
+		red = 0.0;
+		green = 0.0;
+		blue = 0.0;
+		break;
+	case 7: // random
+		isRandom = true;
+		break;
+	}
+}
+
+void processBrushSizeMenu(int value)
+{
+	shape = 5;
+	isEraser = false;
+	brushSize = value;
+	glutSetCursor(GLUT_CURSOR_CROSSHAIR);
+}
+
+void processShapeMenu(int value)
+{
+	shape = value;
+	isEraser = false;
+	isSecond = false;
+
+	switch (shape)
+	{
+	case 1:
+		glutSetCursor(GLUT_CURSOR_RIGHT_ARROW);
+		break;
+	case 2:
+	case 3:
+	case 4:
+		glutSetCursor(GLUT_CURSOR_CROSSHAIR);
+		break;
+	}
+}
+
 void processBorderMenu(int value)
 {
 	isSecond = false;
@@ -81,6 +554,117 @@ void processRadicalBrushMenu(int value)
 {
 	isRadial = value == 1 ? true : false;
 }
+
+void createOurMenu()
+{
+	/*
+    int glutCreateMenu(void (*func)(int value));
+    //glutCreateMenu creates a new pop-up menu.
+func
+The callback function for the menu is called when a menu entry from the menu is selected.
+The value passed to the callback is determined by the value for the selected menu entry.
+    */
+	int colourMenu = glutCreateMenu(processColourMenu); //Creates a Pop-Up Menu
+	glutAddMenuEntry("Red", 1);
+	/*
+	  glutAddMenuEntry adds a menu entry to the bottom of the current menu.
+
+        void glutAddMenuEntry(char *name, int value);
+        name    :      ASCII character string to display in the menu entry.
+        value   :      Value to return to the menu's callback function if the menu entry is selected.
+        Description     :       glutAddMenuEntry adds a menu entry to the bottom of the current menu. The string name will be displayed for the newly added menu entry.
+        If the menu entry is selected by the user,  the menu's callback will be called passing value as the callback's parameter
+	*/
+	glutAddMenuEntry("Green", 2);
+	glutAddMenuEntry("Blue", 3);
+	glutAddMenuEntry("Purple", 4);
+	glutAddMenuEntry("Yellow", 5);
+	glutAddMenuEntry("Black", 6);
+	glutAddMenuEntry("Random", 7);
+
+	int sizeMenu = glutCreateMenu(processBrushSizeMenu);
+	glutAddMenuEntry("4px", 4);
+	glutAddMenuEntry("8px", 8);
+	glutAddMenuEntry("12px", 12);
+	glutAddMenuEntry("16px", 16);
+
+	int sideMenu = glutCreateMenu(processEdgeMenu);
+	glutAddMenuEntry("3", 3);
+	glutAddMenuEntry("4", 4);
+	glutAddMenuEntry("5", 5);
+	glutAddMenuEntry("6", 6);
+	glutAddMenuEntry("7", 7);
+	glutAddMenuEntry("8", 8);
+	glutAddMenuEntry("9", 9);
+	glutAddMenuEntry("10", 10);
+	glutAddMenuEntry("11", 11);
+	glutAddMenuEntry("12", 12);
+	glutAddMenuEntry("15", 15);
+	glutAddMenuEntry("16", 16);
+	glutAddMenuEntry("20", 20);
+
+	int filledMenu = glutCreateMenu(processFilledMenu);
+	glutAddMenuEntry("Fill_Area", 1);
+	glutAddMenuEntry("Unfill_Area", 0);
+
+	int shapeMenu = glutCreateMenu(processShapeMenu);
+	glutAddMenuEntry("Point", 1);
+	glutAddMenuEntry("Line", 2);
+	glutAddSubMenu("Rectangle", filledMenu);
+	glutAddMenuEntry("Circle", 4);
+	glutAddSubMenu("Airbrush", sizeMenu);
+	glutAddSubMenu("Polygon", sideMenu);
+
+	int borderMenu = glutCreateMenu(processBorderMenu);
+	glutAddMenuEntry("2px", 2);
+	glutAddMenuEntry("4px", 4);
+	glutAddMenuEntry("5px", 5);
+	glutAddMenuEntry("6px", 6);
+
+	int eraserSizeMenu = glutCreateMenu(processEraserSizeMenu);
+	glutAddMenuEntry("Small", 2);
+	glutAddMenuEntry("Medium", 6);
+	glutAddMenuEntry("Large", 10);
+
+	int radicalBrushMenu = glutCreateMenu(processRadicalBrushMenu);
+	glutAddMenuEntry("True", 1);
+	glutAddMenuEntry("False", 2);
+
+	glutCreateMenu(processMainMenu);
+	glutAddSubMenu("Colour", colourMenu);
+	glutAddSubMenu("Shapes", shapeMenu);
+	glutAddSubMenu("Border", borderMenu);
+	glutAddSubMenu("Eraser", eraserSizeMenu);
+	glutAddSubMenu("Radical Paint Brush", radicalBrushMenu);
+	glutAddMenuEntry("Undo", 2);
+	glutAddMenuEntry("Redo", 3);
+	glutAddMenuEntry("Clear", 1);
+	glutAddMenuEntry("Quit", 0);
+	glutAttachMenu(GLUT_RIGHT_BUTTON);
+
+	/*
+	glutAttachMenu attaches a mouse button for the current window to the identifier of the current menu;
+	glutDetachMenu detaches an attached mouse button from the current window.
+	By attaching a menu identifier to a button, the named menu will be popped up when the user presses the specified button.
+	button should be one of GLUT_LEFT_BUTTON, GLUT_MIDDLE_BUTTON, and GLUT_RIGHT_BUTTON.
+	Note that the menu is attached to the button by identifier, not by reference.
+	*/
+	/*
+        glutAddSubMenu adds a sub-menu trigger to the bottom of the current menu.
+
+void glutAddSubMenu(char *name, int menu);
+name    :   ASCII character string to display in the menu item from which to cascade the sub-menu.
+menu    :   Identifier of the menu to cascade from this sub-menu menu item.
+
+Description
+
+glutAddSubMenu adds a sub-menu trigger to the bottom of the current menu.
+The string name will be displayed for the newly added sub-menu trigger.
+If the sub-menu trigger is entered, the sub-menu numbered menu will be cascaded, allowing sub-menu menu items to be selected.
+    */
+}
+
+//Event HandLing Zone
 
 void display(void)
 {
